23c8b8dbe98cdf86cca80dc133b8a075a0042c47
xTurbito <miguelalcocer2102@gmail.com>
2024-03-27 00:57:22 -0600

    se cambia el navbar

--- layout/foot.php
+++ layout/foot.php
@@ -1,39 +1,10 @@
-</section>
+
 <!-- Bootstrap JavaScript Libraries -->
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
 </script>
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
 </script>
-
-
 <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-
-<script src="http://localhost/ivephp/Assets/funcionesProductos.js"></script>
-<script src="http://localhost/ivephp/Assets/funcionesUsuarios.js"></script>
-<script src="http://localhost/ivephp/Assets/funcionesDepartamentos.js"></script>
-<script src="http://localhost/ivephp/Assets/funcionesReportes.js"></script>
-
-<script>
-  $(document).ready(function() {
-    $("#tabla_id").DataTable({
-      "pageLength": 5,
-      lengthMenu: [
-        [5, 10, 25, 50],
-        [5, 10, 25, 50],
-      ],
-      "language": {
-        "url": "https://cdn.datatables.net/plug-ins/1.13.1/i18n/es-ES.json"
-      }
-    });
-
-  });
-</script>
-
-
-
-
-
+<script src="http://localhost/encuesta/layout/main.js"></script>
 </body>
-
 </html>
--- modulos/Partidos/index.php
+++ modulos/Partidos/index.php
@@ -6,35 +6,37 @@ $sql = "SELECT * FROM partidos";
 $result = $link->query($sql);
 ?>
 <br>
-<div class="card">
-    <div class="card-header">
-        <h3 class="mt-2 mb-2">Partidos</h3>
-    </div>
-    <div class="card-body p-2"> <!-- Aquí puedes ajustar el padding según tus necesidades -->
-        <div class="table-responsive">
-            <table class="table table-sm" id="tabla_id">
-                <thead>
-                    <tr>
-                        <th scope="col">Nombre</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php 
-                    if ($result->num_rows > 0) {
-                        while ($row = $result->fetch_assoc()) { ?>
-                            <tr>
-                                <td><?php echo $row['Nombre'] ?></td>
-                            </tr>
-                        <?php
+<div class="container">
+    <br>
+    <div class="card w-auto"> <!-- Agrega la clase "text-center" aquí -->
+        <div class="card-header">
+            <h3 class="mt-2 mb-2">Partidos</h3>
+        </div>
+        <div class="card-body"> 
+            <div class="table-responsive-sm">
+                <table class="table" id="tabla_id">
+                    <thead>
+                        <tr>
+                            <th scope="col">Nombre</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php 
+                        if ($result->num_rows > 0) {
+                            while ($row = $result->fetch_assoc()) { ?>
+                                <tr>
+                                    <td scope="row"><?php echo $row['Nombre'] ?></td>
+                                </tr>
+                            <?php
+                            }
+                        } else {
+                            echo "<tr><td colspan='1'>No se encontraron registros.</td></tr>";
                         }
-                    } else {
-                        echo "<tr><td colspan='1'>No se encontraron registros.</td></tr>";
-                    }
-                    ?> 
-                </tbody>
-            </table>
+                        ?> 
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </div>
-
 <?php include("../../layout/foot.php")?>
